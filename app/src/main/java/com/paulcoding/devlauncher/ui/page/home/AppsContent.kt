18583652df7a71fccd81c059248053539c6d3cf0
longnghia <41385034+longnghia@users.noreply.github.com>
2024-11-15 14:43:36 +0700

    Align app items

--- app/src/main/java/com/paulcoding/devlauncher/ui/page/home/AppsContent.kt
+++ app/src/main/java/com/paulcoding/devlauncher/ui/page/home/AppsContent.kt
@@ -1,11 +1,14 @@
 package com.paulcoding.devlauncher.ui.page.home
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -13,9 +16,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import coil3.compose.rememberAsyncImagePainter
+import com.paulcoding.devlauncher.ui.page.AppInfo
 import com.paulcoding.devlauncher.ui.page.ViewModel
 
 @Composable
@@ -26,20 +31,36 @@ fun AppsContent(viewModel: ViewModel) {
         modifier = Modifier
             .fillMaxSize()
     ) {
-        Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(horizontal = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
             uiState.apps.map { app ->
-                val imageBitmap = rememberAsyncImagePainter(app.icon)
-                Row {
-                    Image(
-                        painter = imageBitmap,
-                        "icon",
-                        modifier = Modifier
-                            .width(32.dp)
-                            .height(32.dp)
-                    )
-                    Text(app.name)
-                }
+                AppView(app = app)
             }
         }
+    }
+}
+
+@Composable
+fun AppView(app: AppInfo) {
+    val imageBitmap = rememberAsyncImagePainter(app.icon)
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        Image(
+            painter = imageBitmap,
+            "icon",
+            modifier = Modifier
+                .width(32.dp)
+                .height(32.dp)
+        )
+        Text(app.name)
     }
 }
